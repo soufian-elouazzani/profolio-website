935747f5bd9727cccc1c0aa478a16e250f007370
soufian-elouazzani <soufianelouazzani939@gmail.com>
2026-04-21 22:31:42 +0200

    deleting the content of the README file

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,9 +98,6 @@ export const metadata = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID;
-  if (!GA_ID) {
-    throw new Error("Missing Google Analytics ID");
-  }
 
   return (
     <html lang="en" suppressHydrationWarning>
@@ -138,7 +135,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           strategy="afterInteractive"
         />
       </body>
-      <GoogleAnalytics gaId={GA_ID} />
+      {GA_ID ? <GoogleAnalytics gaId={GA_ID} /> : null}
     </html>
   );
 }
